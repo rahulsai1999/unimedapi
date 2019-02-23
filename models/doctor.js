8f127c911e773f9ae381112e8d0cf07ca4b930f2
Rahul Sai <noneuser2183@gmail.com>
2019-02-23 22:17:34 +0530

    doctor schema changed

--- models/doctor.js
+++ models/doctor.js
@@ -1,7 +1,14 @@
 var mongoose = require("mongoose");
 
 var doctorSchema = new mongoose.Schema({
-    name:String
+    name:String,
+    DOB: String,
+    gender:String,
+    yrsexp:String,
+    email: String,
+    username: String,
+    password:String,
+    profileimg:String
 });
 
 module.exports = mongoose.model("Doctor", doctorSchema);
